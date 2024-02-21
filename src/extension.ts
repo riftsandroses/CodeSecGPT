0840768eb2f2a34538ff152fa1a0dbc711b9f9c1
Utkarsh <utkarsh.cpp@gmail.com>
2024-02-21 21:17:29 +0530

    Added OpenAI API Key to Environment Variables

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import OpenAI from "openai";
 
 const prompt = 'ENTER_PRE-DETERMINED_PROMPT_HERE';
 
